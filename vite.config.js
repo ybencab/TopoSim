6e82d411641cb0982ad5a4bb5fd14a7838ba23ab
Yoel <ybencab@gmail.com>
2025-11-06 16:56:55 +0000

    adding tailwind

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [preact()],
+  plugins: [
+    preact(),
+    tailwindcss(),
+  ],
   server: {
     port: 8080,
     host: true,
